1259861ddeff2be7f3aa41df295d303d9632adc9
Richard Ram <richard@richard-ram.com>
2018-09-17 16:28:44 +0200

    Implemented spawn setup

---
+++ Assets/Scripts/ChunkSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkSpawner : MonoBehaviour 
+{
+    [SerializeField]
+    private LevelChunk[] _chunks;
+
+    [SerializeField]
+    private float _spawnThreshold;
+
+    private Transform _player;
+
+	private void Awake()
+	{
+        _player = GameObject.FindWithTag("Player").transform;
+	}
+
+	private void Update()
+	{
+        if (_spawnThreshold > _player.position.x) return;
+
+        SpawnChunk();
+ 	}
+
+    void SpawnChunk()
+    {
+        print("Spawn");
+    }
+
+	private void OnDrawGizmos()
+	{
+        Gizmos.DrawLine(new Vector3(_spawnThreshold, -5, 0), new Vector3(_spawnThreshold, 5, 0));
+	}
+}
